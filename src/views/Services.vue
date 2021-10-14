9d9dfa5dc64f9fd70e02c82f7efcb978aa9c8fee
Defender0345 <dylannel02@gmail.com>
2021-10-14 16:42:43 +0200

    Added Services page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,15 @@
 <template>
    <div class="services">
-      <h1>This is the services page</h1>
+      <h1>The Services page is under development!</h1>
    </div>
 </template>
+
+<script>
+export default {
+   name: 'Services',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/_shared.scss';
+</style>
